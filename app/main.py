7c5fbe7dc028350817f4084e910540340a017ec5
jakramp <jakramp96@gmail.com>
2026-02-26 05:26:04 +0100

    JKR-15 Added core of the fridge app.

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from app.utils.db import Base
 from auth.routes import user_router
 from config import settings
+from fridge.routes import fridge_router
 from recipes.routes import ingredient_router
 
 engine = create_async_engine(settings.DATABASE_URL)
@@ -28,7 +29,7 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-routers = [user_router, ingredient_router]
+routers = [fridge_router, user_router, ingredient_router]
 
 for router in routers:
     app.include_router(router)
