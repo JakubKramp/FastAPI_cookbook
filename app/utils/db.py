0ecbd2d7797511b4a8b87ddefff107c1b7cec80b
jakramp <jakramp96@gmail.com>
2026-03-01 01:09:45 +0100

    JKR-27 Refactored main module.

--- app/utils/db.py
+++ app/utils/db.py
@@ -1,16 +1,15 @@
 from types import AsyncGeneratorType
 
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
-from config import settings
-
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
+from app.main import engine
 
 
 async def get_session() -> AsyncGeneratorType:
     async with AsyncSession(engine) as session:
         yield session
+
 
 class Base(DeclarativeBase):
     pass
