7349be1c9a7d564bd6fc29fe7fe22a0ec2b9630d
jakramp <jakramp96@gmail.com>
2026-03-01 03:49:45 +0100

    JKR-27 Refactored fridge

--- fridge/schemas.py
+++ fridge/schemas.py
@@ -9,5 +9,4 @@ from recipes.tests.test_data.example_data import example_fridge
 class FridgeDetails(BaseModel):
     products: List[CreateProduct]
 
-
     model_config = ConfigDict(json_schema_extra={"example": example_fridge})
