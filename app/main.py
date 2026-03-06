bc7df527eb1a91559446c261510d417bbe0d3636
jakramp <jakramp96@gmail.com>
2026-03-06 06:21:25 +0100

    JKR-90 Updates health check status message.

--- app/main.py
+++ app/main.py
@@ -15,9 +15,7 @@ app = FastAPI()
 
 @app.get("/health-check", status_code=200)
 async def health_check():
-    return {"server_status": "ok"}
-
-
+    return {"server": "ok"}
 
 
 app.add_middleware(
